518f479db9eebd5a75f0b6c4f381e42e2922a5c7
JS-Cormier <32277947+JS-Cormier@users.noreply.github.com>
2020-09-24 19:26:27 -0400

    Simplified code and fixed browser crashing issue

--- ytuf_chrome_extension/js/script.js
+++ ytuf_chrome_extension/js/script.js
@@ -3,16 +3,6 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
 		window.alert("You must be on a YouTube video page for this to work!");
 	} else {
 		embedUrl = activeTab.url.replace("/watch?v=", "/embed/");
-		var html = `
-		<head>
-			<title>${activeTab.title}</title>
-		</head>
-		<body bgcolor='#282828'>
-			<iframe width='100%'
-					height='97%' 
-					src=${embedUrl} />
-		</body>`;
-		var url = `javascript:document.write(\"${html}\")`;
-		chrome.tabs.create({url: url});
+		chrome.tabs.create({url: embedUrl});
 	}
 });
